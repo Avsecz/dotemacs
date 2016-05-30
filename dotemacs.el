e3d596a1eea062bbd8bae3d4aa80a2f87ba3700d
Ziga Avsec <avsec@in.tum.de>
2016-05-30 12:45:38 +0200

    fixed work.org loading problem

--- dotemacs.el
+++ dotemacs.el
@@ -224,7 +224,7 @@
        (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
  '(org-agenda-files
    (quote
-    ("~/droak/notes/work.org" "~/droak/notes/my.org" "~/droak/notes/notes.org" "/data/ouga/home/ag_gagneur/avsec/projects/credase.org" "~/bayesRare/main.org")))
+    ("~/droak/notes/work.org" "~/droak/notes/my.org" "~/droak/notes/notes.org" "~/bayesRare/main.org")))
  '(org-agenda-ndays 7)
  '(org-agenda-overriding-columns-format
    "%40ITEM(Task) %TODO %SCHEDULED %11Effort(Est. Effort){:} %6CLOCKSUM(T-done)  %6CLOSED(Closed)" t)
