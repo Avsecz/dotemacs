14ef20e3967ebb22a4e687a28cd7e896fb72be47
Avsecz <avsec@genzentrum.lmu.de>
2016-07-27 22:34:29 +0200

    moved completely to the company mode

--- dot-emacs-config/auto-complete.el
+++ dot-emacs-config/auto-complete.el
@@ -1,17 +1,23 @@
 ;; auto complete
 
 (install-package 'auto-complete
-		 'company)
+		 'company
+		 'company-quickhelp
+		 )
 ;; 
 
 (require 'auto-complete)
 (require 'auto-complete-config)
 
+
+
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(ac-config-default)
-(global-auto-complete-mode t)
+;; (ac-config-default)
+;; (global-auto-complete-mode t)
+;; (add-hook 'Tex-mode-hook 'auto-complete-mode) ;; je že nastavljeno globalno
+;; (add-hook 'text-mode-hook 'auto-complete-mode)
 
-(add-hook 'Tex-mode-hook 'auto-complete-mode) ;; je že nastavljeno globalno
-(add-hook 'text-mode-hook 'auto-complete-mode)
+(company-quickhelp-mode 1)
+
