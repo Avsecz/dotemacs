2696a9d6ea324e98c5313ac4de2792833d5dc7ae
Avsecz <avsec@genzentrum.lmu.de>
2016-08-07 16:57:25 +0200

    added protobuf

---
+++ dot-emacs-config/protobuf.el
@@ -0,0 +1,9 @@
+;; protobuf-mode
+
+(install-package 'protobuf-mode
+		 'flycheck-protobuf)
+(require 'protobuf-mode)
+
+;; enable flyspell
+(dolist (hook '(protobuf-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
