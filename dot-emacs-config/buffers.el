87ba2526e571fa9692dfac3e47cc7f8a9e6fa6ed
avsecz <zigaavsec@gmail.com>
2017-02-09 11:01:20 +0100

    feat: added buffer-copy-file-path

--- dot-emacs-config/buffers.el
+++ dot-emacs-config/buffers.el
@@ -169,3 +169,28 @@ If WINDOW is the only one in its frame, then `delete-frame' too."
         (delete-frame)
         (delete-window (selected-window)))))
 (global-set-key (kbd "C-c k") 'delete-single-window)
+
+
+(defun buffer-copy-file-path (&optional *dir-path-only-p)
+  "Copy the current buffer's file path or dired path to `kill-ring'.
+Result is full path.
+If `universal-argument' is called first, copy only the dir path.
+URL `http://ergoemacs.org/emacs/emacs_copy_file_path.html'
+Version 2017-01-27"
+  (interactive "P")
+  (let ((-fpath
+         (if (equal major-mode 'dired-mode)
+             (expand-file-name default-directory)
+           (if (buffer-file-name)
+               (buffer-file-name)
+             (user-error "Current buffer is not associated with a file.")))))
+    (kill-new
+     (if *dir-path-only-p
+         (progn
+           (message "Directory path copied: 「%s」" (file-name-directory -fpath))
+           (file-name-directory -fpath))
+       (progn
+         (message "File path copied: 「%s」" -fpath)
+         -fpath )))))
+
+(global-set-key (kbd "C-c M-w") 'buffer-copy-file-path)
